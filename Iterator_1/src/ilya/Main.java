675a295a23cca33b4f9624421a5ac1d9923a47c2
Ilya Shebanov <ivshebanov@gmail.com>
2017-08-09 10:44:33 +0300

    Iterator_1_реализован

--- Iterator_1/src/ilya/Main.java
+++ Iterator_1/src/ilya/Main.java
@@ -3,6 +3,48 @@ package ilya;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayContaner aC = new ArrayContaner();
+        Iterator iterator = aC.getIterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
     }
 }
+
+interface Iterator {
+    boolean hasNext();
+
+    Object next();
+}
+
+interface Container {
+    Iterator getIterator();
+}
+
+class ArrayContaner implements Container {
+
+    String[] arr = {"Masha", "Mariya", "Максим"};
+
+    @Override
+    public Iterator getIterator() {
+        return new ArrayIterator();
+    }
+
+    class ArrayIterator implements Iterator {
+        int index;
+
+        @Override
+        public boolean hasNext() {
+            return arr.length > index;
+        }
+
+        @Override
+        public Object next() {
+            if (hasNext()) {
+                return arr[index++];
+            }
+            return null;
+        }
+    }
+}
+
