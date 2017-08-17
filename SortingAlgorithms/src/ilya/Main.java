42c1f54c6ef8e9ff0044ecbb906c893e4470765b
Ilya Shebanov <ivshebanov@gmail.com>
2017-08-17 16:41:18 +0300

    Доработал сортировку ВСТАВКАМИ

--- SortingAlgorithms/src/ilya/Main.java
+++ SortingAlgorithms/src/ilya/Main.java
@@ -7,25 +7,19 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-
-
         ArrayList<SortingAlgorithms> list = new ArrayList<>();
         list.add(new BubbleSort());
         list.add(new BubbleSort1());
         list.add(new SampleSort());
         list.add(new InsertSort());
         list.add(new QuickSort());
-
         list.forEach(e -> e.timeMeasurement(getArray()));
-//        list.forEach(e -> e.sort(getArray()));
-//        System.out.println(Arrays.toString(list.get(0).sort(getArray())));
     }
 
     private static int[] getArray() {
 //        return new int[]{1, 9, 5, 0, 7, 2, 9, 4, 8, 10};
-//        return new int[]{1, 3, 4, 6, 7, 2, 8, 5, 9};
-//        return new int[]{1, 2, 3, 4, 5, 6, 7, 2, 5};
-        return new int[]{2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3};
+        return new int[]{1, 3, 4, 6, 7, 2, 8, 5, 9};
+//        return new int[]{2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3, 2, 1, 4, 8, 7, 5, 6, 3};
 //        int[] array = new int[10000];
 //        for (int i = 0; i < array.length; i++) {
 //            array[i] = (int) (Math.random() * 10000);
